27a688fd5b3ed21db8648d41ca8c3c5c6388b79b
Melodie <gardosemelodie@gmail.com>
2023-12-30 16:48:34 +0800

    add search function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const container = document.getElementById("container")
+const myForm = document.getElementById("myForm")
+const search = document.getElementById("search")
+
+
+
+myForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const value = search.value
+    searchMovies(value)
+})
+
 const getMovies = async () => {
     const response = await fetch(
         'https://api.themoviedb.org/3/movie/upcoming?api_key=1bfdbff05c2698dc917dd28c08d41096&language=en-US&page=1'
@@ -17,8 +28,17 @@ const getMovies = async () => {
 
 const searchMovies = async (title) => {
     const response = await fetch(
-        'https://api.themoviedb.org/3/search/movie?api_key=1bfdbff05c2698dc917dd28c08d41096&query=the+avengers'
+        `https://api.themoviedb.org/3/search/movie?api_key=1bfdbff05c2698dc917dd28c08d41096&query=${title}`
     );
+    const mymovies = await response.json()
+    container.innerHTML = mymovies.results.map((data)=>(
+        `
+        <div>
+            <img src=http://image.tmdb.org/t/p/w500/${data.poster_path}>
+            <h1>${data.title}</h1>
+        </div>
+        `
+    )).join("")
 };
 
 getMovies()
